b5fd1193e3bb6bf560b4621bcc29879deff21dfd
Inthrayuth Mahaphol and John Klein <john@>
2016-05-10 11:58:16 -0700

    fail the apptest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,14 +6,16 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    // staticFileLocation("/public");
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //
-    //   model.put("template", "templates/index.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("categories", Category.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,8 +1,9 @@
 import org.fluentlenium.adapter.FluentTest;
-import org.junit.ClassRule;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,9 +18,21 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test
-  // public void rootTest() {
-  //
-  // }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void rootTest() {
+  goTo("http://localhost:4567/");
+  assertThat(pageSource()).contains("Library");
+  }
+
+  @Test
+  public void categoriesIsDisplayedTest() {
+  Category testCategory = new Category("Cartoon");
+  testCategory.save();
+  goTo("http://localhost:4567/");
+  assertThat(pageSource()).contains("Cartoo");
+ }
 
 }
